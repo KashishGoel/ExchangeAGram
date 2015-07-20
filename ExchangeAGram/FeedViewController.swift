89a1ea978b38fe27742e81a6e1d2bec21fa74da2
Kashish Goel <kashishgoel@Kashishs-MacBook-Pro.local>
2015-07-20 15:14:40 -0400

    second commit

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -100,4 +100,18 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UIImagePic
         return cell
     }
     
+    //UICollectionViewDelegate
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let thisItem = feedArray[indexPath.row] as FeedItem
+        var filterVC = FilterViewController ()
+        filterVC.thisFeedItem = thisItem
+        
+        self.navigationController?.pushViewController(filterVC, animated: false)
+        
+        
+    }
+    
+   
+    
+    
 }
---
+++ ExchangeAGram/filterCell.swift
@@ -0,0 +1,27 @@
+//
+//  filterCell.swift
+//  ExchangeAGram
+//
+//  Created by Kashish Goel on 2015-07-19.
+//  Copyright (c) 2015 Kashish Goel. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+  var imageView = UIImageView ()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        contentView.addSubview(imageView)
+}
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    
+}
